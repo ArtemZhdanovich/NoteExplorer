e3503242d08689134060bc4715800747702fbee4
ArtemZhdanovich <artem.zhdanboro@gmail.com>
2025-01-19 15:17:46 +0300

    Revert "comm"
    This reverts commit d76df5ac8993f5779437fd1e49aacfc6697c40f8.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,24 +5,4 @@ builder.Services.AddDataAccess();
 var app = builder.Build();
 
 
-app.MapControllers();
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.Run(async (context) =>
-{
-    context.Response.ContentType = "text/html; charset=utf-8";
-
-    if (context.Request.Path == "/postuser")
-    {
-        var form = context.Request.Form;
-        string name = form["name"];
-        string age = form["age"];
-        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
-    }
-    else 
-    {
-        await context.Response.SendFileAsync("html/index.html");
-    }
-});
 app.Run();
